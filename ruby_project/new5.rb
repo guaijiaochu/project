390ac8306a88f4ee23ceabfccaebaae46bea4aae
guaijiaochu <31947651+guaijiaochu@users.noreply.github.com>
2018-03-09 18:17:33 +0800

    创建new5

---
+++ ruby_project/new5.rb
@@ -0,0 +1,17 @@
+# 导入必要的包
+require 'rubygems'
+require 'irb'
+require 'selenium-webdriver'
+
+# 打开火狐浏览器
+dr=Selenium::WebDriver.for :ff
+
+# 打开淘宝网
+dr.get "http://www.taobao.com"
+
+dr.find_element(:id,'q').send_key "华为手机"
+dr.find_element(:class,'search-button').click
+
+# 退出
+sleep 2
+dr.quit
