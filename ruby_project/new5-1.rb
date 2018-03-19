b709d96b56ea9d441a4ea9926e867d39b16a8ba1
guaijiaochu <31947651+guaijiaochu@users.noreply.github.com>
2018-03-19 16:28:47 +0800

    新增new5-1,new5-2,note1
    新增new5-1,new5-2,准备用POM
    questions 需要查阅理解的问题
    note出现错误并且解决
    note1记录笔记

---
+++ ruby_project/new5-1.rb
@@ -0,0 +1,66 @@
+# 采用pageobject设计模式，改进new5 
+
+# 导入必要的包，打开xx浏览器，并最大化，输入目标网址
+
+# 导入必要的包
+#coding:utf-8
+require "selenium-webdriver"
+require "irb"
+
+
+
+#打开火狐浏览器
+# @dr= Selenium::WebDriver.for :chrome
+
+
+def browser browser
+
+	if browser == "ie"
+		@dr= Selenium::WebDriver.for :ie
+
+	elsif browser == "ff"
+		@dr= Selenium::WebDriver.for :ff
+
+	elsif browser == "chrome"
+		@dr= Selenium::WebDriver.for :chrome
+	else
+		@dr= Selenium::WebDriver.for :chrome
+		
+		
+	end
+	max
+
+end
+
+
+
+# browser "chrome"
+
+
+
+def max
+#浏览器窗口最大化
+@dr.manage.window.maximize
+end
+
+
+
+
+def website website
+	# 打开目标网址
+	@dr.get website
+
+	#为了方便查看结果，等待
+	sleep 1
+
+	# 关闭本次执行打开的页面
+	#@dr.close
+
+	# 关闭所有页面
+	@dr.quit
+
+end
+
+
+
+
---
+++ ruby_project/new5-2.rb
@@ -0,0 +1,15 @@
+#coding:utf-8
+require "selenium-webdriver"
+require "irb"
+# require "new5-1"
+
+# 执行测试用例
+
+# 调用另一个文件的内容
+load "new5-1.rb"
+
+
+# browser "chrome"
+browser "ff"
+# browser "ie"
+website "https://www.taobao.com"
