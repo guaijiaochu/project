ea6d7f7325f4675d6898eb73b9f96035b9e4f3fb
guaijiaochu <31947651+guaijiaochu@users.noreply.github.com>
2020-05-29 18:23:56 +0800

    表达式的类型转换

---
+++ java_project/Hello/src/hellopackage/TypeConvert6_17.java
@@ -0,0 +1,22 @@
+package hellopackage;
+
+public class TypeConvert6_17 {
+
+	public static void main(String[] args) {
+//***表达式的类型转换
+		/*
+		 * 1、占用字节较少的数据类型转换成占用字节较多的数据类型 
+		 * 2、字符类型会转换成int类型
+		 * 3、int类型转换为float类型
+		 * 4、表达式中某个操作数的类型是double，则另一个操作数的类型也会转换为double 
+		 * 5、布尔类型 不能 转换成其他类型
+		 */
+		char ch = 'a' ;
+		short a = -2;
+		int b = 3;
+		float f = 5.3f;
+				
+
+	}
+
+}
