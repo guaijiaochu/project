a17283deaba22af0b4ec77b21bab8ddc4fd78a8f
guaijiaochu <31947651+guaijiaochu@users.noreply.github.com>
2020-05-29 18:09:16 +0800

    增加注释

--- java_project/Hello/src/hellopackage/HelloWorld.java
+++ java_project/Hello/src/hellopackage/HelloWorld.java
@@ -1,7 +1,7 @@
 package hellopackage;
 
 public class HelloWorld {//类：就是具备某些共同特征的实体的集合，它是一种抽象的数据类型，它是对所具有相同特征实体的抽象。在面向对象的程序设计语言中，类是对一类“事物”的属性与行为的抽象。
-//HelloWorld及快捷键
+//***HelloWorld及快捷键
 	public static void main(String[] args) {//方法：完成某项功能的代码，依赖某个特定的对象。如"调用对象X的Y方法"，而不能"调用Y方法",方法和对象相关，而函数和对象无关
 //		为啥是从后往前输出？
 		System.out.println("hello");//println自动换行
