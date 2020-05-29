b1b9ecacfa2a07c7df7b87b45299b57d76450d85
guaijiaochu <31947651+guaijiaochu@users.noreply.github.com>
2020-05-29 18:16:11 +0800

    新增注释

--- java_project/Hello/src/hellopackage/RelationExpression6_14.java
+++ java_project/Hello/src/hellopackage/RelationExpression6_14.java
@@ -3,7 +3,7 @@ package hellopackage;
 public class RelationExpression6_14 {
 
 	public static void main(String[] args) {
-//		简单的关系表达式的使用
+//***简单的关系表达式的使用
 		int a =5,b=4;
 		boolean t1=a>b;
 		boolean t2=a==b;
