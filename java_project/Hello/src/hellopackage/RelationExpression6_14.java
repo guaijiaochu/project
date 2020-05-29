93c4899dfc425abf06a4bc4770d91fa05ab41d8c
guaijiaochu <31947651+guaijiaochu@users.noreply.github.com>
2020-05-29 18:09:00 +0800

    简单的关系表达式的使用

---
+++ java_project/Hello/src/hellopackage/RelationExpression6_14.java
@@ -0,0 +1,14 @@
+package hellopackage;
+
+public class RelationExpression6_14 {
+
+	public static void main(String[] args) {
+//		简单的关系表达式的使用
+		int a =5,b=4;
+		boolean t1=a>b;
+		boolean t2=a==b;
+		System.out.println("a大于b:"+t1+"\n\t"+"a=b:"+t2);
+
+	}
+
+}
