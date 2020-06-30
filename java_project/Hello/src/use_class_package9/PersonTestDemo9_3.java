72c3ad2152cd9e807931f3541bbfe125d56c0574
guaijiaochu <31947651+guaijiaochu@users.noreply.github.com>
2020-06-30 11:36:10 +0800

    Person类的对象调用类中是属性和方法

---
+++ java_project/Hello/src/use_class_package9/PersonTestDemo9_3.java
@@ -0,0 +1,13 @@
+package use_class_package9;
+
+public class PersonTestDemo9_3 {
+//接9_1，Person类的对象调用类中是属性和方法
+	
+	public static void main(String[] args) {
+		Person9_1 p = new Person9_1();
+		p.name = "张三";
+		p.age = 25;
+		p.talk();
+	}
+
+}
