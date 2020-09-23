c4cc09340f5173255b2caa0eadd965e3ddfb7b51
guaijiaochu <31947651+guaijiaochu@users.noreply.github.com>
2020-09-23 11:07:13 +0800

    47创建新项目

--- python_project/Stupid Python/01/46.py
+++ python_project/Stupid Python/01/46.py
@@ -1,4 +1,50 @@
 # 
 # 习题46项目骨架  ----测试你的配置127
 
- 
+#  项目骨架  ----测试你的配置---工具相关
+# 
+#         pycharm汉化
+#         https://blog.csdn.net/qq_42825420/article/details/105690039
+#         建议方法3
+#         
+#         pycharm上传git
+#         https://blog.csdn.net/john_bian/article/details/94657057
+#         https://www.cnblogs.com/wcwnina/p/9112255.html
+#         
+#         git下载慢
+#         https://blog.csdn.net/guanjungao/article/details/103265751
+#         
+#         怎样配置eclipse中忽略某些不必要提交的文件
+#         https://jingyan.baidu.com/article/455a9950b448b0a1662778cc.html
+#         
+#         ＧＣＣ下载
+#         https://www.jianshu.com/p/dc0fc5d8c900
+#         
+#         codeblocks下载（下自带编译器的）
+#         https://blog.csdn.net/ysz171360154/article/details/84572114
+#         改变了默认安装地址，将安装地址选到了E盘，之后
+#         安装完成后需要改一下编译器位置。
+#         settings-complier-Toolchain executables -修改编译器位置
+#         
+#         
+#         python相关软件包下载地址
+#         https://pypi.org/
+#         接下来你需要安装下面的软件包： 
+#         1. pip – http://pypi.python.org/pypi/pip 
+#         2. distribute – http://pypi.python.org/pypi/distribute 
+#         3. nose – http://pypi.python.org/pypi/nose/ 
+#         4. virtualenv – http://pypi.python.org/pypi/virtualenv
+#         windows 的pip安装步骤
+#         在如上网站下载安装包tar.gz格式
+#         1、双击pip-19.3.1.tar.gz安装包，在打开的解压窗口中点击解压到图标进行解压缩
+#         2、解压缩后，进入pip-19.3.1目录下，可以看到解压缩之后的文件
+#         3、打开命令提示符窗口
+#         4、输入cd命令进入pip-19.3.1所在路径下，输入命令dir可以看到目录下的所有文件列表
+#         5、 执行命令python setup.py install 开始安装
+#         6、输入pip有使用帮助回显说明安装成功。到此在windows上安装pip的方法就介绍完了
+#         
+#         .whl为安装包
+#         安装方法  pip install xxx.whl
+#         
+#         nose python测试工具
+#         https://www.cnblogs.com/sunshine-blog/p/6823049.html
