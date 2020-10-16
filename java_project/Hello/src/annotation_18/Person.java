135ae70ae4abe740e9432125e4fb0c87b8d743a5
guaijiaochu <31947651+guaijiaochu@users.noreply.github.com>
2020-10-16 14:30:32 +0800

    Annotation给编译器看的注释

---
+++ java_project/Hello/src/annotation_18/Person.java
@@ -0,0 +1,13 @@
+package annotation_18;
+
+/*Annotation表示注释的语法，可以将所有的配置写入程序中。
+内建的annotation
+@Override：表示方法是属于覆写操作
+@Suppress Warnings：压制警告*/
+
+
+public class Person {
+	public String say() {
+		return "人说话";
+	}
+}
---
+++ java_project/Hello/src/annotation_18/Student.java
@@ -0,0 +1,8 @@
+package annotation_18;
+
+public class Student extends Person {
+	@Override//表示覆写操作
+	public String say() {
+		return "学生说话";
+	}
+}
